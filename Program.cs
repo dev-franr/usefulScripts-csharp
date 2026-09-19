8f925091e13117020fe9fa2aa72aaae4dbacfb9f
franr <dev.franr@gmail.com>
2026-09-19 13:12:05 +0100

    Over engineer the reason I added this script. Just need to input the current devs and MZ project folders and it will copy the files over.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 ﻿using usefulScripts_csharp;
 
-var directoryA = string.Empty;
-var directoryB = string.Empty;
-FileTransfer.FromDirectoryAToDirectoryB(directoryA, directoryB);
+// Base folders for Rider projects and RPGMMZ projects.
+const string riderProjects = "riderprojects";
+const string rpgmmz = "rpgmmz";
+
+// Edit these when either project changes.
+var currentDev = Path.Combine("rpgmmzPlugin-Quests", "js", "dev");
+var currentMZProject = Path.Combine("SeptemberProject");
+
+CopyCurrentRpgmmzPlugInFilesToProjectFolder(currentDev, currentMZProject);
+
+return;
+
+static void CopyCurrentRpgmmzPlugInFilesToProjectFolder(string currentDevFolder, string currentMZProjectFolder)
+{
+    var pathA = Path.Combine(riderProjects, currentDevFolder);
+    var pathB = Path.Combine(rpgmmz, currentMZProjectFolder);
+    FileTransfer.CopyFilesFromPathAToPathB(pathA, pathB);
+}
