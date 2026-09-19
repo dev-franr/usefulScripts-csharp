61ce14f7539746af86907b6de9e0e53ee6c873be
franr <dev.franr@gmail.com>
2026-09-19 13:03:28 +0100

    Rename FileTransfer.FromDirectoryAToDirectoryB to CopyFilesFromPathAToPathB.

--- FileTransfer.cs
+++ FileTransfer.cs
@@ -2,8 +2,26 @@ namespace usefulScripts_csharp;
 
 public static class FileTransfer
 {
-    public static void FromDirectoryAToDirectoryB(string directoryA, string directoryB)
+    // TODO: configure naming conventions.
+    private const string RootFolderPath = "/home/deck/Desktop";
+    private const char FileSeparator = '/';
+
+    public static void CopyFilesFromPathAToPathB(string pathA, string pathB)
     {
-        
+        var currentDirectory = Environment.CurrentDirectory;
+        Environment.CurrentDirectory = RootFolderPath;
+        var filesLocation = Path.Combine(RootFolderPath, pathA);
+        var fileList = Directory.GetFiles(filesLocation);
+        foreach ( var file in fileList )
+        {
+            var separated = file.Split(FileSeparator);
+            var fileName = separated.Last();
+            var fileToCopy = Path.Combine(filesLocation, file);
+            var copyTo = Path.Combine(pathB, fileName);
+            File.Copy(fileToCopy, copyTo, true);
+        }
+
+        // Not sure if needed.
+        Environment.CurrentDirectory = currentDirectory;
     }
 }
