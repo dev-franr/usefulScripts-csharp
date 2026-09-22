b607deb6fa12c31f51d7357984c96dbf3a653438
franr <dev.franr@gmail.com>
2026-09-22 19:56:24 +0100

    Revery previous change, there are only js files here anyway.

--- FileTransfer.cs
+++ FileTransfer.cs
@@ -11,7 +11,7 @@ public static class FileTransfer
         var currentDirectory = Environment.CurrentDirectory;
         Environment.CurrentDirectory = RootFolderPath;
         var filesLocation = Path.Combine(RootFolderPath, pathA);
-        var fileList = Directory.GetFiles(filesLocation, ".js");
+        var fileList = Directory.GetFiles(filesLocation);
 
         if (!Directory.Exists(pathB))
             Directory.CreateDirectory(pathB);
